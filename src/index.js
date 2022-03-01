ae75831ff7e05bb17f65a6562eb0f7891e4d46f9
Santiago Fleitas <sansfleitas@gmail.com>
2022-03-01 18:38:32 -0300

    Resolving all problems

--- src/index.js
+++ src/index.js
@@ -23,14 +23,18 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const { url, title, techs } = request.body;
+  const updatedRepository = { url, title, techs };
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -48,7 +52,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -67,8 +71,10 @@ app.post("/repositories/:id/like", (request, response) => {
   }
 
   const likes = ++repositories[repositoryIndex].likes;
+  const repository = { ...repositories[repositoryIndex], likes }
+  repositories[repositoryIndex] = repository;
 
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
